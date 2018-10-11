827bc3d1099ceb465f3eff41af80ad8e7f6c947d
Alexandre Magno Monteiro <alexandre.magno@forlogic.net>
2018-10-11 17:51:20 -0300

    track value react 16 cypress

--- MetaProjetoExemplo.AcceptanceTests/cypress/integration/projects.spec.js
+++ MetaProjetoExemplo.AcceptanceTests/cypress/integration/projects.spec.js
@@ -7,9 +7,41 @@ context('Project Management', () => {
 
     cy.wait('@projects')
     cy.get('input[name="title"]').type('testing')
-    cy.get('input[name="startDate"]').type('10102020')
-    cy.get('input[name="finishDate"]').type('10102020')
-    
+    // cy.get('input[name="startDate"]').clear().type('10102020')
+    // cy.get('input[name="finishDate"]').clear().focus().type('10102020')
+    // cy.wait(1000)
+
+
+    cy.get('input[name="finishDate"]')
+      .as('finishDate')
+      .then($input => {
+      // get native DOM element
+        $input = $input.get(0);
+
+        // track last value since we need it for React16 track value
+        const lastValue = $input.value;
+
+        $input.value = '29102018';
+
+        // dispatch change into React as well
+        $input._valueTracker.setValue(lastValue);
+        $input.dispatchEvent(new Event('change', { bubbles: true }));
+      })
+    cy.get('input[name="startDate"]')
+      .as('startDate')
+      .then($input => {
+    // get native DOM element
+      $input = $input.get(0);
+
+      // track last value since we need it for React16 track value
+      const lastValue = $input.value;
+
+      $input.value = '29102018';
+
+      // dispatch change into React as well
+      $input._valueTracker.setValue(lastValue);
+      $input.dispatchEvent(new Event('change', { bubbles: true }));
+    })
 
     // cy.contains('button', 'Criar').click();
   })
